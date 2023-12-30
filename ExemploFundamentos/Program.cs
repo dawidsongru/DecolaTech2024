b41d2d4f4532712631e1025523e1ef994056d801
Dawidson Pereira <dawidsongru@gmail.com>
2023-12-30 08:17:38 -0300

    Incremento e Decremento

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -3,14 +3,38 @@
 
 Calculadora calc = new Calculadora();    // Instanciar uma classe. Criar uma varável do tipo calculadora.
 
-// calc.Somar(10, 30);                      // a vírcula delimita cada parâmetro. Ex: (10, 30)
+// calc.Somar(10, 30);                      // a vírgula delimita cada parâmetro. Ex: (10, 30)
 // calc.Subtrair(10, 50);
 // calc.Multiplicar(15, 45);
 // calc.Dividir(2, 2);
 // calc.Potencia(3, 3);
-calc.Seno(30);
-calc.Coseno(30);
-calc.Tangente(30);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+
+int numeroIncremento = 10;
+
+Console.WriteLine(numeroIncremento);
+
+Console.WriteLine("Incrementando o 10");
+// numeroIncremento = numeroIncremento + 1;               // código longo. Incrementar em 1
+numeroIncremento++;                                       // código curto. Incrementar em 1
+
+Console.WriteLine(numeroIncremento);
+
+
+int numeroDecremento = 20;
+
+Console.WriteLine(numeroDecremento);
+
+Console.WriteLine("Decrementando o 20");
+// numeroDecremento = numeroDecremento - 1;               // código longo. Decrementar em 1
+numeroDecremento--;                                       // código curto. Decrementar em 1
+
+Console.WriteLine(numeroDecremento);
+
+
+
 
 
 
