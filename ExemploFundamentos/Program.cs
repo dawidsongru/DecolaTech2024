9e4dcf5c71e9b0217c0e4f637e574248832b79b0
Dawidson Pereira <dawidsongru@gmail.com>
2023-12-30 12:21:59 -0300

    Introdução ao WHILE

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,10 +1,13 @@
 ﻿using ExemploFundamentos.Models;
 
+    
 int numero = 5;
+int contador = 0;               // contador começando em 0
 
-for(int contador = 0; contador <=10; contador++)
+while (contador <= 10)          // Enquanto uma condição for verdadeira, execute o código. While só tem a condição.
 {
-    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+    Console.WriteLine($"{contador}º Execução: {numero} x {contador} = {numero * contador}");
+    contador++;
 }
 
 
@@ -13,11 +16,12 @@ for(int contador = 0; contador <=10; contador++)
 
 
 
+// int numero = 5;
 
-
-
-
-
+// for(int contador = 0; contador <=10; contador++)        // for = para
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
 
 
 
