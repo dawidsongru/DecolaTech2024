6e972bcb7df0d51d0e34247f2d693a4c5f903d98
Dawidson Pereira <dawidsongru@gmail.com>
2023-12-30 07:42:40 -0300

    Funções Trigonométricas

--- ExemploFundamentos/Models/Calculadora.cs
+++ ExemploFundamentos/Models/Calculadora.cs
@@ -29,8 +29,29 @@ namespace ExemploFundamentos.Models
         
         public void Potencia(int x, int y)
         {   
-            double pot = Math.Pow(x, y);
+            double pot = Math.Pow(x, y);                // Pow = Power = Potência
             Console.WriteLine($"{x} ^ {y} = {pot}");
+        }
+
+        public void Seno(double angulo)                 // Sin = Sine = Seno
+        {                                     // primeiro divisão Math.PI / 180, depois multiplicação angulo * Math.PI
+            double radiano = angulo * Math.PI / 180;    // Seno é calculado em Radianos. Para calcular Radianos, pegamos PI (3,141) / 180. Convertido.
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}º = {Math.Round(seno, 4)}");   // Math.Round arredonda o número com a quantidade de dígitos desejada, 4.
+        }
+
+        public void Coseno(double angulo)                 // Cos = Coseno
+        {
+            double radiano = angulo * Math.PI / 180;    
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo}º = {Math.Round(coseno, 4)}");    // Math.Round arredonda o número com a quantidade de dígitos desejada, 4.
+        }
+        
+        public void Tangente(double angulo)                 // Tan = Tangente
+        {
+            double radiano = angulo * Math.PI / 180;    
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo}º = {Math.Round(tangente, 4)}");    // Math.Round arredonda o número com a quantidade de dígitos desejada, 4.
         }
     }
 }
