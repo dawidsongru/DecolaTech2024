56e29639ff2adb0b87c7fb4bd0dbd3014e9ec5f0
Dawidson Pereira <dawidsongru@gmail.com>
2023-12-31 20:12:10 -0300

    Criando um método

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorando.Models;         // Importar o namespace que está dentro de Models
+
+Pessoa p1 = new Pessoa();               // Instanciada uma classe Pessoa. Variável p1 do tipoo Pessoa.
+p1.Nome = "";
+p1.Idade = -1;
+p1.Apresentar();
