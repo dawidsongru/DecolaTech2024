ddad6e0e5bbcc29c56ba31a82b9012b1d9d1fc11
Dawidson Pereira <dawidsongru@gmail.com>
2024-01-01 02:33:43 -0300

    Validações no GET e SET

--- ExemploExplorando/Models/Pessoa.cs
+++ ExemploExplorando/Models/Pessoa.cs
@@ -7,7 +7,27 @@ namespace ExemploExplorando.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
+        // Não queremos que o nome seja vazio.
+        private string _nome;                   // criar um campo do tipo privado
+        
+        public string Nome
+        {
+            get                                 // Obter valor.
+            {
+                return _nome.ToUpper();         // Retornar um nome com todas as letras maiúsculas.
+            }
+
+            set                                 // Atribuir valor.
+            {
+                if (value == "")                // se valor for igual a vazio. O value é o argumento que está recebendo o nome.
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");    // disparar uma exceção
+                }
+
+                _nome = value;
+            }
+        }
+ 
         public int Idade { get; set; }
 
         public void Apresentar()
