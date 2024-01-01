9fc99b0c5ddad26ecd9e788ca404ce2349ba1c2e
Dawidson Pereira <dawidsongru@gmail.com>
2024-01-01 04:00:36 -0300

    Validando a propriedade idade

--- ExemploExplorando/Models/Pessoa.cs
+++ ExemploExplorando/Models/Pessoa.cs
@@ -8,15 +8,22 @@ namespace ExemploExplorando.Models
     public class Pessoa
     {
         // Não queremos que o nome seja vazio.
-        private string _nome;                   // criar um campo do tipo privado
+        private string _nome;               // criar um campo _nome do tipo privado
+        private int _idade;                 // criar um campo _idade do tipo privado
         
         public string Nome
         {
-            get => _nome.ToUpper();
-            
-            set                                 // Atribuir valor.
+
+        get => _nome.ToUpper();     // Body Expression: Uma maneira mais resumida no GET e SET para seu código ficar mais legível.
+        
+        // get
+        // {
+        //     return _nome.ToUpper();      // Se o seu código tiver só uma linha, é preferível usar o Body Expression.
+        // }
+
+            set                             // Atribuir valor.
             {
-                if (value == "")                // se valor for igual a vazio. O value é o argumento que está recebendo o nome.
+                if (value == "")            // se valor for igual a vazio. O value é o argumento que está recebendo o nome.
                 {
                     throw new ArgumentException("O nome não pode ser vazio");    // disparar uma exceção
                 }
@@ -25,7 +32,20 @@ namespace ExemploExplorando.Models
             }
         }
  
-        public int Idade { get; set; }
+        public int Idade
+        {
+            get => _idade;          // Body Expression: Uma maneira mais resumida no GET e SET para seu código ficar mais legível.
+            // get;                 // Se o seu código tiver só uma linha, é preferível usar o Body Expression.
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que zero");
+                }
+
+                _idade = value;
+            }
+        }
 
         public void Apresentar()
         {
--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,6 +1,6 @@
 ﻿using ExemploExplorando.Models;         // Importar o namespace que está dentro de Models
 
 Pessoa p1 = new Pessoa();               // Instanciada uma classe Pessoa. Variável p1 do tipoo Pessoa.
-p1.Nome = "";
-p1.Idade = -1;
+p1.Nome = "Dawidson";
+p1.Idade = 20;
 p1.Apresentar();
